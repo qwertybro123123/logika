7d2498059745ce385f0635dc803bf9a2dcd0fcde
qwertybro123123 <132608959+qwertybro123123@users.noreply.github.com>
2023-08-03 19:34:24 +0300

    Add files via upload

---
+++ logika/m2/u1/Crazy people/main.py
@@ -0,0 +1,37 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from random import randint
+
+s = True
+
+app = QApplication([])
+main_window = QWidget()
+
+buton = QPushButton("Скачати 1221 троянів")
+text = QLabel("Натисни щоб скачати майнкрафт 2 без регистрації і смс")
+winner = QLabel("?")
+
+
+line = QVBoxLayout()
+line.addWidget(text)
+line.addWidget(winner)
+line.addWidget(buton)
+
+def win():
+    global s
+    if s == True:
+        ran = randint(1, 1000) 
+        winner.setText(str(ran))
+        s=False
+buton.clicked.connect(win)
+
+
+main_window.setLayout(line)
+
+
+
+
+
+
+main_window.show()
+app.exec_()
